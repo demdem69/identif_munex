a618f2e49735251a83a47ca2decb7684db65eddd
demdem69 <dementhon.louis@gmail.com>
2026-02-03 14:45:21 -0500

    Delete constants.tsx

--- constants.tsx
+++
@@ -1,6 +0,0 @@
-
-import { Ordnance } from './types';
-
-// La base de données statique est désormais vide. 
-// Tout le catalogue est construit dynamiquement à partir du dossier /MunDB
-export const ORDNANCE_DATA: Ordnance[] = [];
